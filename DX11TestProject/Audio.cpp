d10bb32c4cdf3e4be586b2e94f51dda0be1a48d0
Carlos Lugo <clugo@fullsail.com>
2018-08-28 14:51:22 -0400

    updating the includes for AK added stdafx.h as precompiled header

--- DX11TestProject/Audio.cpp
+++ DX11TestProject/Audio.cpp
@@ -1,6 +1,23 @@
 #include "Audio.h"
 
+#include <AK/SoundEngine/Common/AkMemoryMgr.h>									// Memory Manager
+#include <AK/SoundEngine/Common/AkModule.h>										// Default memory and stream managers
+#include <AK/SoundEngine/Common/IAkStreamMgr.h>									// Streaming Manager
+#include <AK/MusicEngine/Common/AkMusicEngine.h>
+#include <AK/Tools/Common/AkPlatformFuncs.h>									// Thread defines
+#include <AK\IBytes.h>
+#include <AK/SoundEngine/Common/AkStreamMgrModule.h>
+#include <AK/SoundEngine/Common/AkSoundEngine.h>
+#include <AK/SoundEngine/Common/AkStreamMgrModule.h>
+//#include "..\DX11TestProject\Dependencies\Wwise\Sample\Wwise_IDs.h"
+#include "Dependencies/Wwise/Sample/SoundEngine/Win32/AkFilePackageLowLevelIOBlocking.h"
 
+#include <DirectXMath.h>
+
+#define INIT_BANK "Assets/Soundbanks/Init2.bnk"
+#define DEFAULT_BANK "Assets/Soundbanks/New_SoundBank.bnk"
+
+CAkFilePackageLowLevelIOBlocking g_lowLevelIO;
 
 CAudio::CAudio()
 {
@@ -10,3 +27,98 @@ CAudio::CAudio()
 CAudio::~CAudio()
 {
 }
+
+bool CAudio::Init() 
+{
+	AkMemSettings memSettings;
+	memSettings.uMaxNumPools = 20;
+
+	AK::MemoryMgr::Init(&memSettings);
+
+
+	//
+	// Create and initialize an instance of the default streaming manager. Note
+	// that you can override the default streaming manager with your own. Refer
+	// to the SDK documentation for more information.
+	//
+
+	AkStreamMgrSettings stmSettings;
+	AK::StreamMgr::GetDefaultSettings(stmSettings);
+
+	// Customize the Stream Manager settings here.
+
+	if (!AK::StreamMgr::Create(stmSettings))
+	{
+		assert(!"Could not create the Streaming Manager");
+		return false;
+	}
+
+	//
+	// Create a streaming device with blocking low-level I/O handshaking.
+	// Note that you can override the default low-level I/O module with your own. Refer
+	// to the SDK documentation for more information.      
+	//
+	AkDeviceSettings deviceSettings;
+	AK::StreamMgr::GetDefaultDeviceSettings(deviceSettings);
+
+	// Customize the streaming device settings here.
+
+	// CAkFilePackageLowLevelIOBlocking::Init() creates a streaming device
+	// in the Stream Manager, and registers itself as the File Location Resolver.
+	if (g_lowLevelIO.Init(deviceSettings) != AK_Success)
+	{
+		assert(!"Could not create the streaming device and Low-Level I/O system");
+		return false;
+	}
+
+
+	//
+	// Create the Sound Engine
+	// Using default initialization parameters
+	//
+
+	AkInitSettings initSettings;
+	AkPlatformInitSettings platformInitSettings;
+	AK::SoundEngine::GetDefaultInitSettings(initSettings);
+	AK::SoundEngine::GetDefaultPlatformInitSettings(platformInitSettings);
+
+	if (AK::SoundEngine::Init(&initSettings, &platformInitSettings) != AK_Success)
+	{
+		assert(!"Could not initialize the Sound Engine.");
+		return false;
+	}
+
+	//
+	// Initialize the music engine
+	// Using default initialization parameters
+	//
+
+	AkMusicSettings musicInit;
+	AK::MusicEngine::GetDefaultInitSettings(musicInit);
+
+	if (AK::MusicEngine::Init(&musicInit) != AK_Success)
+	{
+		assert(!"Could not initialize the Music Engine.");
+		return false;
+	}
+	return true;
+}
+
+bool CAudio::Update()
+{
+	return true;
+}
+
+bool CAudio::Shutdown()
+{
+	return true;
+}
+
+void CAudio::setListenerPosition(const DirectX::XMFLOAT4X4* cameraMatrix)
+{
+}
+bool CAudio::LoadBanks()
+{
+	return true;
+}
+
--- DX11TestProject/Audio.h
+++ DX11TestProject/Audio.h
@@ -1,22 +1,19 @@
 #ifndef _AUDIO_H_
 #define _AUDIO_H_
 
-#include <AK/SoundEngine/Common/AkTypes.h>
+#include "Camera.h"
+#include "BaseObject.h"
+#include <vector>
+#include "Defines.h"
 
-#include <AK/SoundEngine/Common/AkMemoryMgr.h>		// Memory Manager
-#include <AK/SoundEngine/Common/AkModule.h>			// Default memory and stream managers
-#include <AK/SoundEngine/Common/IAkStreamMgr.h>		// Streaming Manager
-#include <AK/SoundEngine/Common/AkSoundEngine.h>    // Sound engine
-#include <AK/MusicEngine/Common/AkMusicEngine.h>	// Music Engine
-#include <AK/SoundEngine/Common/AkStreamMgrModule.h>	// AkStreamMgrModule
-#include <AK/SpatialAudio/Common/AkSpatialAudio.h>	// Spatial Audio module
-
-#include <AK/SoundEngine/Common/AkSoundEngine.h>
-#include <AK/IBytes.h>
-
-#ifndef AK_OPTIMIZED
-#include <AK/Comm/AkCommunication.h>
+#if !_DEBUG
+#define AK_OPTIMIZED
 #endif
+
+namespace DirectX
+{
+	struct XMFLOAT4X4;
+}
 
 class CAudio
 {
@@ -24,9 +21,57 @@ public:
 	CAudio();
 	~CAudio();
 
-	void Init() {}
-	void Update() {}
-	void Shutdown() {}
+	bool Init();
+	bool Update();
+	bool Shutdown();
+
+	void setListenerPosition(const DirectX::XMFLOAT4X4* cameraMatrix);
+	bool LoadBanks();
+	//void registerObject(EventMessageBase* e);
+
+private:
+	const DirectX::XMFLOAT4X4* audioListener;
+	std::vector<BaseObject*> objectList;
 };
+
+
+/////////////////////////////////////////////////////////////////////////////////
+// Custom alloc/free functions. These are declared as "extern" in AkMemoryMgr.h
+// and MUST be defined by the game developer.
+/////////////////////////////////////////////////////////////////////////////////
+namespace AK
+{
+#ifdef WIN32
+	void * AllocHook(size_t in_size)
+	{
+		return malloc(in_size);
+	}
+	void FreeHook(void * in_ptr)
+	{
+		free(in_ptr);
+	}
+	// Note: VirtualAllocHook() may be used by I/O pools of the default implementation
+	// of the Stream Manager, to allow "true" unbuffered I/O (using FILE_FLAG_NO_BUFFERING
+	// - refer to the Windows SDK documentation for more details). This is NOT mandatory;
+	// you may implement it with a simple malloc().
+	void * VirtualAllocHook(
+		void * in_pMemAddress,
+		size_t in_size,
+		DWORD in_dwAllocationType,
+		DWORD in_dwProtect
+	)
+	{
+		return VirtualAlloc(in_pMemAddress, in_size, in_dwAllocationType, in_dwProtect);
+	}
+	void VirtualFreeHook(
+		void * in_pMemAddress,
+		size_t in_size,
+		DWORD in_dwFreeType
+	)
+	{
+		VirtualFree(in_pMemAddress, in_size, in_dwFreeType);
+	}
+#endif
+}
 
 #endif
